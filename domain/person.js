26ba0d732e63023d65f355465e1ab04dfb1b7aa6
Rick Hamers <rawhamer@student.avans.nl>
2018-04-26 13:22:30 +0200

    Added comments and GET,PUSH and semi functional DELETE functionality

---
+++ domain/person.js
@@ -0,0 +1,22 @@
+class Person {
+
+    //Consttructor for person
+    constructor(firstname, lastname){
+        this._firsname = firstname;
+        this._lastname = lastname;
+    }
+
+    //Getter for firstname
+    getfirstname(){
+        return this._firsname;
+    }
+
+    //getter for lastname
+    getlastname(){
+        return this._lastname
+    }
+}
+
+//Make person available to other classes
+module.exports = Person;
+
--- server.js
+++ server.js
@@ -1,15 +1,120 @@
+// _-//TypicalDoofus\\-_
+// A basic NodeJS Server
 //
-// A basic NodeJS Server //TypicalDoofus\\
-//
-let express = require('express');
-let app = express();
 
+//Require for the used modules
+const express = require('express');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+//Importing the person.js class
+const person = require('./domain/person');
+
+//Initializing the Express module
+const app = express();
+
+//Setting the port for the NodeJS server
 const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.send('TypicalDoofus');
+//Defining the modules used in the Express server
+app.use(morgan('dev'));
+app.use(bodyparser.json());
+
+//Making a personList Array
+let personList = [];
+
+//Parsing the request
+app.use('*', (req, res, next) => {
+    let httpMethod = req.method;
+    let requestUrl = req.baseUrl;
+    console.log('We received a ' + httpMethod + ' request at ' + requestUrl);
+
+
+    next();
 });
 
+//The GET personList request
+app.get('/api/person', (req, res) => {
+    console.log('---------------A GET request was made---------------');
+
+    const newPerson = new person('Typical', 'Doofus'); //Making a new Person object
+
+    res.status(200).json(personList).end(); //Response to the GET request
+});
+
+//The GET person request
+app.get('/api/person/:id', (req, res, next) => {
+    console.log('---------------A GET request was made---------------');
+
+    const id = req.params.id;  //Requesting the ID for the person object
+
+    if (id >= 0 && id < personList.length){
+        //The ID is correct - Send the person back
+        res.status(200).json(personList).end(); //Response to the GET request
+    } else {
+        //The ID is not correct - Error
+        const error = {
+            error: 'ID does not exist (index out of bounds)',
+            url: req.baseUrl,
+            statuscode: 404
+        };
+        next(error);
+    }
+
+});
+
+//The POST request
+app.post('/api/person', (req, res) => {
+    console.log('---------------A POST request was made---------------');
+
+    console.log(req.body); //Printing the POST request's body
+    const firstname = req.body.firstname; //Requesting firstname from the client
+    const lastname = req.body.lastname; //Requesting lastname from the client
+
+    const postPerson = new person(firstname, lastname); //Making a new person using the posted firstname and lastname
+    personList.push(postPerson); // adding the postPerson to the personList
+
+    //response to the POST request
+    res.status(200).json(postPerson).end();//Response to the POST request
+});
+
+//The DELETE request
+app.delete('/api/person/:number', (req, res) => {
+    console.log('---------------A DELETE request was made---------------');
+
+    console.log(req.body); //Printing the POST request's body
+    const number = req.params.number; //Requesting firstname from the client
+
+    personList.splice(number,1); // adding the postPerson to the personList
+
+    //response to the DELETE request
+    res.status(200).json(personList).end();//Response to the POST request
+});
+
+//No endpoint found
+app.use('*', (req, res, next) => {
+    let httpMethod = req.method;
+    let requestUrl = req.baseUrl;
+    console.log('We received a ' + httpMethod + ' request at ' + requestUrl);
+
+    //Creating a response for error 404 : Page not found
+    const error = {
+        error: 'Endpoint does not exist',
+        url: requestUrl
+    };
+    //Sending error info to Final error Handler
+    next(error);
+});
+
+// Final error handler for Next(Info);
+app.use((err,req,res,next) => {
+    console.log('Final error handler: an error occurred');
+    console.log(err);
+
+    //Responding to the error
+    res.status(500).json(err).end();
+});
+
+//Starting the server
 app.listen(port, () => {
     console.log('The server is running on port ' + port)
 });
